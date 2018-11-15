b55d596ccc206c158e80ed0b3d661e11c27e66f3
Luis Ovando <isc.luisovando@gmail.com>
2018-11-14 20:02:28 -0600

    Add domain model (User)

---
+++ src/main/java/com/luisovando/springboot/app/domain/model/User.java
@@ -0,0 +1,65 @@
+package com.luisovando.springboot.app.domain.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.katharsis.resource.annotations.JsonApiId;
+import io.katharsis.resource.annotations.JsonApiResource;
+
+@JsonApiResource(type="users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@JsonApiId
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private Date createdAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
